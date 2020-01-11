b8a4ec86dae52556eae9f266f442c12500fd918a
suraj <surajkhayamali.sk@gmail.com>
2020-01-11 13:52:49 +0545

    Ticket:Error Handling  5ae9b76a703c7c603202ef22

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -326,8 +326,10 @@ export default class MoviesDAO {
 
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
-      console.error(`Something went wrong in getMovieByID: ${e}`)
-      throw e
+      if (String(e).startsWith("MongoError: E11000 duplicate key error")) {
+        return null
+      }
+      return null
     }
   }
 }
